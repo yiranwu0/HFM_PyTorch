eacc6ecbac34c97282646b6039d4703686525c67
Yiran Wu <32823396+kevin666aa@users.noreply.github.com>
2020-12-22 11:28:17 +0800

    Add files via upload

---
+++ Source/Cylinder2D_flower_systematic.py
@@ -0,0 +1,247 @@
+import numpy as np
+import math
+import scipy.io
+import time
+import sys
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class HFM(object):
+    # notational conventions
+    # _tf: placeholders for input/output data and points used to regress the equations
+    # _pred: output of neural network
+    # _eqns: points used to regress the equations
+    # _data: input-output data
+    # _star: preditions
+    
+    def __init__(self, data, c_data, eqns, layers, Pec, Rey):
+        # specs & flow properties
+        self.layers = layers
+        self.Pec = Pec
+        self.Rey = Rey
+        # data
+        [self.data, self.c_data, self.eqns] = [data, c_data, eqns]
+        # physics "uninformed" neural networks
+        self.net = neural_net(self.layers)
+        
+    def compute_loss(self, batch_size):
+        N_data = self.data.shape[0]
+        N_eqns = self.eqns.shape[0]
+        
+        idx_data = np.random.choice(N_data, min(batch_size, N_data))
+        idx_eqns = np.random.choice(N_eqns, batch_size)
+        
+        (data_batch, c_data_batch, eqns_batch) = (self.data[idx_data,:],
+                                                  self.c_data[idx_data,:],
+                                                  self.eqns[idx_eqns,:])
+
+        # predict and split
+        [c_data_pred,_,_,_] = torch.split(self.net(data_batch), 1,1)
+        
+        [c_eqns_pred, u_eqns_pred, v_eqns_pred, p_eqns_pred] = torch.split(self.net(eqns_batch), 1,1)
+        
+        [e1_eqns_pred, e2_eqns_pred, e3_eqns_pred, e4_eqns_pred] = \
+            Navier_Stokes_2D(c_eqns_pred, u_eqns_pred, v_eqns_pred, p_eqns_pred,
+                             eqns_batch, self.Pec, self.Rey)
+        
+        # loss
+        loss = mean_squared_error(c_data_pred, c_data_batch)  + \
+                mean_squared_error(e1_eqns_pred, torch.zeros_like(e1_eqns_pred)) + \
+                mean_squared_error(e2_eqns_pred, torch.zeros_like(e1_eqns_pred)) + \
+                mean_squared_error(e3_eqns_pred, torch.zeros_like(e1_eqns_pred)) + \
+                mean_squared_error(e4_eqns_pred, torch.zeros_like(e1_eqns_pred))
+        
+        return loss
+
+        
+    def test(self, T_star, X_star, Y_star, C_star, U_star, V_star, P_star):
+        snap = np.array([100])
+        t_test = T_star[:,snap]
+        x_test = X_star[:,snap]
+        y_test = Y_star[:,snap]
+
+        c_test = C_star[:,snap]
+        u_test = U_star[:,snap]
+        v_test = V_star[:,snap]
+        p_test = P_star[:,snap]
+
+            # Prediction
+        c_pred, u_pred, v_pred, p_pred = self.predict(t_test, x_test, y_test)
+
+        error_c = relative_error(c_pred, torch.from_numpy(c_test).float())
+        error_u = relative_error(u_pred, torch.from_numpy(u_test).float())
+        error_v = relative_error(v_pred, torch.from_numpy(v_test).float())
+        error_p = relative_error(p_pred - torch.mean(p_pred), torch.from_numpy(p_test - np.mean(p_test)).float())
+
+        print('Error c: %e' % (error_c))
+        print('Error u: %e' % (error_u))
+        print('Error v: %e' % (error_v))
+        print('Error p: %e' % (error_p))
+        
+        
+    def train(self, epochs, batch_size, lr, T_star, X_star, Y_star, C_star, U_star, V_star, P_star):
+        optimizer = Adam(self.net.parameters(), lr)
+        
+        for i in range(epochs):
+            optimizer.zero_grad()
+            loss = self.compute_loss(batch_size)
+            loss.backward()
+            optimizer.step()
+            print("Epochs: " + str(i) + "    Loss: " + str(loss))
+            
+            if(i%100 == 0):
+                self.test(T_star, X_star, Y_star, C_star, U_star, V_star, P_star)
+    
+    def predict(self, t_star, x_star, y_star):
+        t = torch.from_numpy(t_star).float()
+        x = torch.from_numpy(x_star).float()
+        y = torch.from_numpy(y_star).float()
+        data = torch.cat((t, x, y), 1)
+        
+        [c_star, u_star, v_star, p_star] = torch.split(self.net(data),1,1)
+        return c_star, u_star, v_star, p_star
+
+
+def main():
+    # model parameters
+    batch_size = 10000
+    layers = [3] + 10*[4*50] + [4]
+    lr = 1e-3
+    epochs = 1000
+    
+    # Load Data
+    data = scipy.io.loadmat('HFM-master/Data/Cylinder2D_flower.mat')
+    
+    t_star = data['t_star'] # T x 1
+    x_star = data['x_star'] # N x 1
+    y_star = data['y_star'] # N x 1
+    
+    T = t_star.shape[0]
+    N = x_star.shape[0]
+        
+    U_star = data['U_star'] # N x T
+    V_star = data['V_star'] # N x T
+    P_star = data['P_star'] # N x T
+    C_star = data['C_star'] # N x T
+    
+    # Rearrange Data
+    T_star = np.tile(t_star, (1,N)).T # N x T
+    X_star = np.tile(x_star, (1,T)) # N x T
+    Y_star = np.tile(y_star, (1,T)) # N x T
+    
+    ######################################################################
+    ######################## Training Data ###############################
+    ######################################################################
+    
+    #T_data = int(sys.argv[1])
+    #N_data = int(sys.argv[2])
+
+    T_data = 201
+    N_data = 15000
+    
+    idx_t = np.concatenate([np.array([0]), np.random.choice(T-2, T_data-2, replace=False)+1, np.array([T-1])] )
+    idx_x = np.random.choice(N, N_data, replace=False)
+    t_data = T_star[:, idx_t][idx_x,:].flatten()[:,None]
+    x_data = X_star[:, idx_t][idx_x,:].flatten()[:,None]
+    y_data = Y_star[:, idx_t][idx_x,:].flatten()[:,None]
+    c_data = C_star[:, idx_t][idx_x,:].flatten()[:,None]
+        
+    T_eqns = T
+    N_eqns = N
+    idx_t = np.concatenate([np.array([0]), np.random.choice(T-2, T_eqns-2, replace=False)+1, np.array([T-1])] )
+    idx_x = np.random.choice(N, N_eqns, replace=False)
+    t_eqns = T_star[:, idx_t][idx_x,:].flatten()[:,None]
+    x_eqns = X_star[:, idx_t][idx_x,:].flatten()[:,None]
+    y_eqns = Y_star[:, idx_t][idx_x,:].flatten()[:,None]
+    
+    t_data = Variable(torch.from_numpy(t_data).float(), requires_grad = True)
+    x_data = Variable(torch.from_numpy(x_data).float(), requires_grad = True)
+    y_data = Variable(torch.from_numpy(y_data).float(), requires_grad = True)
+    c_data = Variable(torch.from_numpy(c_data).float(), requires_grad = False)
+    
+    t_eqns = Variable(torch.from_numpy(t_eqns).float(), requires_grad = True)
+    x_eqns = Variable(torch.from_numpy(x_eqns).float(), requires_grad = True)
+    y_eqns = Variable(torch.from_numpy(y_eqns).float(), requires_grad = True)
+    
+    data = torch.cat((t_data, x_data, y_data), 1)
+    eqns = torch.cat((t_eqns, x_eqns, y_eqns), 1)
+    
+    
+    # get model and train
+    model = HFM(data, c_data, eqns, layers, Pec = 100, Rey = 100)
+    model.train(epochs, batch_size, lr)
+
+    
+    # Test Data
+    snap = np.array([100])
+    t_test = T_star[:,snap]
+    x_test = X_star[:,snap]
+    y_test = Y_star[:,snap]
+    
+    c_test = C_star[:,snap]
+    u_test = U_star[:,snap]
+    v_test = V_star[:,snap]
+    p_test = P_star[:,snap]
+    
+    # Prediction
+    c_pred, u_pred, v_pred, p_pred = model.predict(t_test, x_test, y_test)
+    
+    # Error
+    error_c = relative_error(c_pred, c_test)
+    error_u = relative_error(u_pred, u_test)
+    error_v = relative_error(v_pred, v_test)
+    error_p = relative_error(p_pred - np.mean(p_pred), p_test - np.mean(p_test))
+
+    print('Error c: %e' % (error_c))
+    print('Error u: %e' % (error_u))
+    print('Error v: %e' % (error_v))
+    print('Error p: %e' % (error_p))
+    
+    
+    ################# Save Data ###########################
+    
+    C_pred = 0*C_star
+    U_pred = 0*U_star
+    V_pred = 0*V_star
+    P_pred = 0*P_star
+    for snap in range(0,t_star.shape[0]):
+        t_test = T_star[:,snap:snap+1]
+        x_test = X_star[:,snap:snap+1]
+        y_test = Y_star[:,snap:snap+1]
+        
+        c_test = C_star[:,snap:snap+1]
+        u_test = U_star[:,snap:snap+1]
+        v_test = V_star[:,snap:snap+1]
+        p_test = P_star[:,snap:snap+1]
+    
+        # Prediction
+        c_pred, u_pred, v_pred, p_pred = model.predict(t_test, x_test, y_test)
+        
+        C_pred[:,snap:snap+1] = c_pred
+        U_pred[:,snap:snap+1] = u_pred
+        V_pred[:,snap:snap+1] = v_pred
+        P_pred[:,snap:snap+1] = p_pred
+    
+        # Error
+        error_c = relative_error(c_pred, c_test)
+        error_u = relative_error(u_pred, u_test)
+        error_v = relative_error(v_pred, v_test)
+        error_p = relative_error(p_pred - np.mean(p_pred), p_test - np.mean(p_test))
+    
+        print('Error c: %e' % (error_c))
+        print('Error u: %e' % (error_u))
+        print('Error v: %e' % (error_v))
+        print('Error p: %e' % (error_p))
+    
+    scipy.io.savemat('../Results/Cylinder2D_flower_results_%d_%d_%s.mat' %(T_data, N_data, time.strftime('%d_%m_%Y')),
+                     {'C_pred':C_pred, 'U_pred':U_pred, 'V_pred':V_pred, 'P_pred':P_pred})
+ 
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Source/utilities.py
@@ -0,0 +1,94 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+loss = nn.MSELoss()
+
+def relative_error(pred, exact):
+    if type(pred) is np.ndarray:
+        return np.sqrt( mean_squared_error(pred, exact)/np.mean(np.square(exact - np.mean(exact))))
+    
+    return torch.sqrt( loss(pred, exact) / torch.mean(torch.square(exact - torch.mean(exact))) )
+
+def mean_squared_error(pred, exact):
+    if type(pred) is np.ndarray:
+        return np.mean(np.square(pred - exact))
+    return loss(pred, exact)
+
+def fwd_gradients(Y, x):
+    dummy = torch.ones_like(Y)
+    G = torch.autograd.grad(Y, x, dummy, create_graph=True)[0]
+    return G
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+class neural_net(nn.Module):
+    def __init__(self, layer_dim):
+        super().__init__()
+        
+        temp = []
+        for l in range(1, len(layer_dim)):
+            temp.append(nn.Linear(layer_dim[l-1], layer_dim[l]))
+        self.layers = nn.ModuleList(temp)
+        print(self.layers)
+        sys.stdout.flush()
+        
+    def forward(self, x):
+        for l in self.layers:
+            x = swish(l(x))
+        return x
+
+
+def Navier_Stokes_2D(c, u, v, p, txy, Pec, Rey):
+
+    # gradients w.r.t each output and all inputs
+    c_txy = fwd_gradients(c, txy)
+    u_txy = fwd_gradients(c, txy)
+    v_txy = fwd_gradients(c, txy)
+    p_txy = fwd_gradients(c, txy)
+    
+    #---wanted
+   # print(c_txy)
+    c_t = c_txy[:,0:1]
+    c_x = c_txy[:,1:2]
+    c_y = c_txy[:,2:3]
+    
+    u_t = u_txy[:,0:1]
+    u_x = u_txy[:,1:2]
+    u_y = u_txy[:,2:3]
+    
+    v_t = v_txy[:,0:1]
+    v_x = v_txy[:,1:2]
+    v_y = v_txy[:,2:3]
+    
+    p_x = p_txy[:,1:2]
+    p_y = p_txy[:,2:3]
+    #wanted----
+    
+    # second gradient
+   # print(c_x)
+   # print(txy)
+    c_x_txy = fwd_gradients(c_x, txy)
+    c_y_txy = fwd_gradients(c_y, txy)
+    c_xx = c_x_txy[:,1:2] #wanted
+    c_yy = c_y_txy[:,2:3] #wanted
+    
+    u_x_txy = fwd_gradients(u_x, txy)
+    u_y_txy = fwd_gradients(u_y, txy)
+    u_xx = u_x_txy[:,1:2] #wanted
+    u_yy = u_y_txy[:,2:3] #wanted
+    
+    v_x_txy = fwd_gradients(v_x, txy)
+    v_y_txy = fwd_gradients(v_y, txy)
+    v_xx = v_x_txy[:,1:2] #wanted
+    v_yy = v_y_txy[:,2:3] #wanted
+    
+    e1 = c_t + (u*c_x + v*c_y) - (1.0/Pec)*(c_xx + c_yy)
+    e2 = u_t + (u*u_x + v*u_y) + p_x - (1.0/Rey)*(u_xx + u_yy)
+    e3 = v_t + (u*v_x + v*v_y) + p_y - (1.0/Rey)*(v_xx + v_yy)
+    e4 = u_x + v_y
+    
+    return e1, e2, e3, e4
+
+
